40fd63709410467fe866e1436367a19ad0fa6b7b
thangtechdaily <thangtechdaily@users.noreply.github.com>
2013-11-26 13:45:36 +0700

    minify html output in php

---
+++ minify.php
@@ -0,0 +1,21 @@
+<?php
+#Place this file in root folder of your website (ex: /var/home/public_html)
+#Open php.ini file and find auto_prepend_file
+#Paste link to file (ex: auto_prepend_file = "/var/home/public_html/minify.php")
+#origin: http://jesin.tk/how-to-use-php-to-minify-html-output/
+function minify_output($buffer){
+	$search = array(
+		'/\>[^\S ]+/s',
+		'/[^\S ]+\</s',
+		'/(\s)+/s');
+	$replace = array(
+		'>',
+		'<',
+		'\\1');
+	if (preg_match("/\<html/i",$buffer) == 1 && preg_match("/\<\/html\>/i",$buffer) == 1) {
+		$buffer = preg_replace($search, $replace, $buffer);
+	}
+		return $buffer;
+	}
+ob_start("minify_output");
+?>
